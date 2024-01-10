bc52144388ac7ea209785fbd50efa6123af23cc9
reinaldopabonfuentes <reinaldopabonfuentes@gmail.com>
2024-01-09 21:20:09 -0500

    First Commit

---
+++ src/Summary.java
@@ -0,0 +1,40 @@
+public class Summary {
+    public static void main(String[] args) {
+        /*
+        *
+        * What is Lambda expression
+        *   Lambda is equivalen to a function (method) without a name
+        *   Lambdas are also referred as Anonymous functions
+        *       Method parameters
+        *       Method Body
+        *       Return Type
+        *   Lambdas are not tied to any class like a regular method
+        *   Lambda can also be assigned to variable and passed around
+        *
+        * Usages of Lambda
+        *   Lambda is mainly used to implement Functional Interfaces (SAM)
+        *   Examples:
+        *
+        *   @FunctionalInterface
+        *   public interface Comparator<T>{
+        *       int compare(T o1, T o2);
+        *   }
+        *
+        *   @FunctionalInterface
+        *   public interface Runnable(){
+        *       void run();
+        *  }
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        *
+        *  */
+    }
+}
